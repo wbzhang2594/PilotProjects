75f0d375fe46745199ca1c69315ed3549ac49514
Wenbin Zhang <ght648@motorolasolutions.com>
2015-07-01 19:13:59 +0800

    MockServerReady

---
+++ TestCaseManageServer/Client/TestAgent.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TestCaseManager;
+
+namespace Client
+{
+    public class TestAgent
+    {
+        int agentID;
+        public TestAgent(int id)
+        {
+            agentID = id;
+        }
+
+        public void RunCases(bool NeedMockServer, int CountPerSecond)
+        {
+            ITestCaseManageServer Server = ServerFactory.GetServer(NeedMockServer);
+
+            List<string> caseNameList = GetCaseNameList();
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            foreach (string casename in caseNameList)
+            {
+                if (Server.RequestRunThisCase(casename))
+                {
+                    Thread.Sleep(1000 / CountPerSecond);
+                    //Server.FinishedOneCase(casename, true);
+                }
+            }
+
+            sw.Stop();
+            System.Console.WriteLine(string.Format("TestAgent {0} run {1} ms.", agentID, sw.ElapsedMilliseconds));
+        }
+
+        private List<string> GetCaseNameList()
+        {
+            List<string> result = new List<string>();
+            string filePath = MockBuild.GetTestCaseFilePath();
+
+            using (StreamReader fileReader = new StreamReader(filePath))
+            {
+                string line = fileReader.ReadLine();
+                result.Add(line);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ TestCaseManageServer/MockBuild/MockBuild.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCaseManager
+{
+    public class MockBuild
+    {
+        static string testCaseFilePath = Path.GetFullPath(@"TestCases.txt");
+
+        public static string GetTestCaseFilePath()
+        {
+
+            return testCaseFilePath;
+        }
+    }
+}
