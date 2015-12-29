32a67497d0a1f46e51d45e5cf638739a12b83079
Wenbin Zhang <wbzhang64@hotmail.com>
2015-12-29 23:56:33 +0800

    Add log

--- TRWatcher/WatcherConsole/Program.cs
+++ TRWatcher/WatcherConsole/Program.cs
@@ -6,7 +6,12 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using System.Threading;
+using System.Globalization;
+using log4net.Core;
 
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
 namespace WatcherConsole
 {
     class Program
@@ -16,44 +21,65 @@ namespace WatcherConsole
             try
             {
 
-                WebClient MyWebClient = new WebClient();
+                //WebClient MyWebClient = new WebClient();
 
 
-                MyWebClient.Credentials = CredentialCache.DefaultCredentials;//获取或设置用于向Internet资源的请求进行身份验证的网络凭据
+                //MyWebClient.Credentials = CredentialCache.DefaultCredentials;//获取或设置用于向Internet资源的请求进行身份验证的网络凭据
 
-                Byte[] pageData = MyWebClient.DownloadData(@"http://nebula:3000/api/Failed"); //从指定网站下载数据
+                //Byte[] pageData = MyWebClient.DownloadData(@"http://nebula:3000/api/Failed"); //从指定网站下载数据
 
-                string pageHtml = Encoding.Default.GetString(pageData);  //如果获取网站页面采用的是GB2312，则使用这句            
+                //string pageHtml = Encoding.Default.GetString(pageData);  //如果获取网站页面采用的是GB2312，则使用这句            
 
-                try
+                //try
+                //{
+                //    File.WriteAllText(@"d:\_Test\TRWatcher\FailedCases.json", pageHtml);
+                //}
+                //catch
+                //{
+
+                //}
+                do
                 {
-                    File.WriteAllText(@"d:\_Test\TRWatcher\FailedCases.json", pageHtml);
-                }
-                catch
-                {
+                    DateTime dt = DateTime.Now;
+                    string CurrentCultureInfo = Thread.CurrentThread.CurrentCulture.ToString();
+                    File.AppendAllText("findResult.json", dt.ToString("G", CultureInfo.InvariantCulture));
+                    log4net.LogManager.GetLogger(typeof(Program)).Error(dt.ToString("G", CultureInfo.InvariantCulture));
 
-                }
+                    string pageHtml = File.ReadAllText("FailedCases.json");
 
-                var jArray = JsonConvert.DeserializeObject(pageHtml) as Newtonsoft.Json.Linq.JArray;
-                foreach (var item in jArray)
-                {
-                    string message = item.Value<string>("Message");
-                    string CleanMessage = message.Replace("&nbsp;", " ");
+                    var jArray = JsonConvert.DeserializeObject(pageHtml) as Newtonsoft.Json.Linq.JArray;
 
-                    if (item.Value<string>("Message").Equals("[ThreadSafeIssue]", StringComparison.InvariantCultureIgnoreCase))
+                    Newtonsoft.Json.Linq.JArray findResult = new Newtonsoft.Json.Linq.JArray();
+
+                    foreach (string result in QueryFrom(jArray))
                     {
-                        string FullName = item.Value<string>("FullName");
-                    }
-                    
+                        File.AppendAllText("findResult.json", result);
+                        log4net.LogManager.GetLogger(typeof(Program)).Error(result);
 
-                }
+                    }
+                    Thread.Sleep(1000);
+                } while (true);
+
             }
-            catch
+            catch (Exception ex)
             {
 
             }
 
         }
 
+        private static IEnumerable<string> QueryFrom(Newtonsoft.Json.Linq.JArray jArray)
+        {
+            foreach (var item in jArray)
+            {
+                string message = item.Value<string>("Message");
+                string CleanMessage = message.Replace("&nbsp;", " ");
+
+                if (true /*item.Value<string>("Message").StartsWith("[ThreadSafeIssue]", StringComparison.InvariantCultureIgnoreCase)*/)
+                {
+                    yield return JsonConvert.SerializeObject(item);
+                }
+            }
+        }
     }
 }
