f2c9728cd56d123844c0d833217d48cd1813214f
Wenbin Zhang <ght648@motorolasolutions.com>
2015-12-30 18:54:43 +0800

    20151230

---
+++ TRWatcher/DataContract/TestCaseResult_Key.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataContract
+{
+    public class TestCaseResult_Key : IEqualityComparer<TestCaseResult_Key>
+    {
+        public TestCaseResult_Key(string FullName, string MessageVersion)
+        {
+            this.CaseFullName = FullName;
+            this.MessageVersion = MessageVersion;
+        }
+
+        public string CaseFullName { get; private set; }
+        public string MessageVersion { get; private set; }
+
+
+        public bool Equals(TestCaseResult_Key x, TestCaseResult_Key y)
+        {
+            if (x == null || y == null)
+            {
+                return false;
+            }
+
+            return x.CaseFullName == y.CaseFullName && x.MessageVersion == y.MessageVersion;
+        }
+
+        public int GetHashCode(TestCaseResult_Key obj)
+        {
+            int hash = 17;
+            // Suitable nullity checks etc, of course :)
+            hash = hash * 23 + obj.CaseFullName.GetHashCode();
+            hash = hash * 23 + obj.MessageVersion.GetHashCode();
+            return hash;
+        }
+
+
+
+    }
+}
--- TRWatcher/WatcherConsole/Program.cs
+++ TRWatcher/WatcherConsole/Program.cs
@@ -9,9 +9,9 @@ using Newtonsoft.Json;
 using System.Threading;
 using System.Globalization;
 using log4net.Core;
+using DataContract;
 
-
-[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+[assembly: log4net.Config.XmlConfigurator(ConfigFileExtension = "log4net", Watch = true)]
 namespace WatcherConsole
 {
     class Program
@@ -20,12 +20,59 @@ namespace WatcherConsole
         {
             try
             {
+                List<string> CareList = new List<string>()
+                {
+                    @"BLLTest_PCT:BL.Operation.PostCheckInTest.PCRCompileTest.PCRPackunPack_DebugTest",
+                    @"BLLTest_PCT:BL.Operation.PostCheckInTest.PCRCompileTest.PCRPackunPack_MatRep_R24B",
+                    @"BLLTest_PCT:BL.Operation.PostCheckInTest.PCRCompileTest.PCRPackunPack_Par_R24",
+                    @"BLLTest_PCT:BL.Operation.PostCheckInTest.PCRCompileTest.PCRPackunPack_Par_R24A_AS",
+                    @"BLLTest_PCT:BL.Operation.PostCheckInTest.PCRCompileTest.PCRPackunPack_Par_R251_LA",
+                    @"BLLTest_PCT:BL.Operation.PostCheckInTest.PCRCompileTest.PCRPackunPack_Par_R251",
+                    @"BLLTest_PCT:BL.Operation.PostCheckInTest.PCRCompileTest.PCRPackunPack_Par_R260_AS",
+                    @"BLLTest_PCT:BL.Operation.PostCheckInTest.PCRCompileTest.PCRPackunPack_Par_R260_EMEA",
+                    @"BLLTest_PCT:BL.Operation.PostCheckInTest.PCRCompileTest.PCRPackunPack_Par_R260",
+                    @"BLLTest_PCT:BL.Operation.PostCheckInTest.PCRCompileTest.PCRPackunPack_Par_R260_LA",
+                    @"BLLTest_PCT:BL.Operation.PostCheckInTest.PCRCompileTest.PCRPackunPack_Par_R260_NA",
+                };
 
-                //WebClient MyWebClient = new WebClient();
+                WebClient MyWebClient = new WebClient();
+                MyWebClient.Credentials = CredentialCache.DefaultCredentials;//获取或设置用于向Internet资源的请求进行身份验证的网络凭据
 
+                Newtonsoft.Json.Linq.JArray failedLogs = new Newtonsoft.Json.Linq.JArray();
 
-                //MyWebClient.Credentials = CredentialCache.DefaultCredentials;//获取或设置用于向Internet资源的请求进行身份验证的网络凭据
+                foreach (string caseItem in CareList)
+                {
+                    string QueryString = @"http://nebula:3000/api/TestCases/" + caseItem;
+                    
+                    Byte[] pageData = MyWebClient.DownloadData(QueryString); //从指定网站下载数据
 
+                    string pageHtml = Encoding.Default.GetString(pageData);  //如果获取网站页面采用的是GB2312，则使用这句
+
+                    var jArray = JsonConvert.DeserializeObject(pageHtml) as Newtonsoft.Json.Linq.JArray;
+
+                    foreach (var jSonItem in jArray)
+                    {
+                        if (jSonItem.Value<bool>("Passed") == false)
+                        {
+                            string fullName = jSonItem.Value<string>("FullName");
+                            string OriginalMessage = jSonItem.Value<string>("Message");
+                            string MessageVersion = GetMessageVersion(OriginalMessage);
+
+                            TestCaseResult_Key key = new TestCaseResult_Key(fullName, MessageVersion);
+                        }
+                    }
+
+                    //string cleanedPageHtml = pageHtml.Replace("&nbsp;", " ");
+                    //try
+                    //{
+                    //    File.WriteAllText(@"d:\_Test\TRWatcher\OneCases.json", cleanedPageHtml);
+                    //}
+                    //catch
+                    //{
+
+                    //}
+
+                }
                 //Byte[] pageData = MyWebClient.DownloadData(@"http://nebula:3000/api/Failed"); //从指定网站下载数据
 
                 //string pageHtml = Encoding.Default.GetString(pageData);  //如果获取网站页面采用的是GB2312，则使用这句            
@@ -38,27 +85,29 @@ namespace WatcherConsole
                 //{
 
                 //}
-                do
-                {
-                    DateTime dt = DateTime.Now;
-                    string CurrentCultureInfo = Thread.CurrentThread.CurrentCulture.ToString();
-                    File.AppendAllText("findResult.json", dt.ToString("G", CultureInfo.InvariantCulture));
-                    log4net.LogManager.GetLogger(typeof(Program)).Error(dt.ToString("G", CultureInfo.InvariantCulture));
 
-                    string pageHtml = File.ReadAllText("FailedCases.json");
 
-                    var jArray = JsonConvert.DeserializeObject(pageHtml) as Newtonsoft.Json.Linq.JArray;
+                //do
+                //{
+                //    DateTime dt = DateTime.Now;
+                //    string CurrentCultureInfo = Thread.CurrentThread.CurrentCulture.ToString();
+                //    File.AppendAllText("findResult.json", dt.ToString("G", CultureInfo.InvariantCulture));
+                //    log4net.LogManager.GetLogger(typeof(Program)).Error(dt.ToString("G", CultureInfo.InvariantCulture));
 
-                    Newtonsoft.Json.Linq.JArray findResult = new Newtonsoft.Json.Linq.JArray();
+                //    string pageHtml = File.ReadAllText("FailedCases.json");
 
-                    foreach (string result in QueryFrom(jArray))
-                    {
-                        File.AppendAllText("findResult.json", result);
-                        log4net.LogManager.GetLogger(typeof(Program)).Error(result);
+                //    var jArray = JsonConvert.DeserializeObject(pageHtml) as Newtonsoft.Json.Linq.JArray;
 
-                    }
-                    Thread.Sleep(1000);
-                } while (true);
+                //    Newtonsoft.Json.Linq.JArray findResult = new Newtonsoft.Json.Linq.JArray();
+
+                //    foreach (string result in QueryFrom(jArray))
+                //    {
+                //        File.AppendAllText("findResult.json", result);
+                //        log4net.LogManager.GetLogger(typeof(Program)).Error(result);
+
+                //    }
+                //    Thread.Sleep(1000);
+                //} while (true);
 
             }
             catch (Exception ex)
@@ -68,7 +117,12 @@ namespace WatcherConsole
 
         }
 
-        private static IEnumerable<string> QueryFrom(Newtonsoft.Json.Linq.JArray jArray)
+        public static string GetMessageVersion(string originalMessage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerable<string> QueryFrom(Newtonsoft.Json.Linq.JArray jArray)
         {
             foreach (var item in jArray)
             {
