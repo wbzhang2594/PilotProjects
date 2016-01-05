b8ae96570b61b6d88106664ce7e56fdbd22d0403
Wenbin Zhang <ght648@motorolasolutions.com>
2016-01-05 15:07:25 +0800

    C_Sharp_newTech

---
+++ C_Sharp_newTech/NumericMultiCulture/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumericMultiCulture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //double dNum = -2222222.2222222;
+            int dNum = -1;
+            Dictionary<string, HashSet<CultureInfo>> Dic_strNum_ListCultureInfo = new Dictionary<string, HashSet<CultureInfo>>();
+            
+            foreach (var cI in CultureInfo.GetCultures(CultureTypes.AllCultures))
+            {
+                string strNum = string.Format(cI, "{0}", dNum); //dNum.ToString(cI);
+                string strNum2 = string.Format(CultureInfo.InvariantCulture, "{0}", dNum); //dNum.ToString(cI);
+                //string strNum3 = string.Format("{0}", dNum); //dNum.ToString(cI);
+
+                if (Dic_strNum_ListCultureInfo.ContainsKey(strNum)==false)
+                {
+                    Dic_strNum_ListCultureInfo[strNum] = new HashSet<CultureInfo>();
+                }
+
+                Dic_strNum_ListCultureInfo[strNum].Add(cI);
+
+            }
+
+            foreach (var NumFormat in Dic_strNum_ListCultureInfo.Keys)
+            {
+                System.Console.WriteLine(NumFormat);
+
+                StringBuilder sb = new StringBuilder();
+                foreach (var cI in Dic_strNum_ListCultureInfo[NumFormat])
+                {
+                    sb.Append(cI.ToString() + ";  ");
+                }
+                
+                System.Console.WriteLine("    => " + sb.ToString());
+            }
+        }
+    }
+}
---
+++ C_Sharp_newTech/ThreadSafeTest/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadSafeTest
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int ThreadCount = 64;
+            ManualResetEvent[] donEvent = new ManualResetEvent[ThreadCount];
+            Dictionary<string, string> dic_map = new Dictionary<string, string>();
+            ThreadSafeTestClass testclass = new ThreadSafeTestClass(dic_map);
+
+
+            for (int i = 0; i < 10000; i++)
+            {
+                dic_map["key_" + i.ToString()] = "value_" + i;
+            }
+
+            for (int i = 0; i < ThreadCount; i++)
+            {
+                donEvent[i] = new ManualResetEvent(false);
+
+                ThreadPool.QueueUserWorkItem(testclass.ThreadPoolCallBack, donEvent[i]);
+            }
+
+            WaitHandle.WaitAll(donEvent);
+            System.Console.WriteLine("Finished.");
+            System.Console.ReadKey();
+        }
+
+    }
+
+    class ThreadSafeTestClass
+    {
+        Dictionary<string, string> dic_map;
+
+        public ThreadSafeTestClass(Dictionary<string, string> dic_map)
+        {
+            this.dic_map = dic_map;
+        }
+
+        public void ThreadPoolCallBack(object state)
+        {
+            ManualResetEvent donEvent = (ManualResetEvent)state;
+
+            for (int count = 0; count < 10000; count++)
+            {
+                string key = "key_" + count;
+                if(!dic_map.ContainsKey(key))
+                {
+                    System.Console.WriteLine("Found no key issue.");
+                }
+                Thread.Sleep(1);
+            }
+            System.Console.WriteLine("Thread Finished.");
+            donEvent.Set();
+        }
+    }
+}
