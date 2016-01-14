f4aa1596e277390c937f163436ed5c90e2d397c2
Wenbin Zhang <ght648@motorolasolutions.com>
2016-01-14 18:06:36 +0800

    2016/01/14

---
+++ TRWatcher/WatcherConsole/QueryStatement/StatementHandler/IStatementHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WatcherConsole.QueryStatement
+{
+    interface IStatementHandler
+    {
+        bool HandleStatement(IStatement statement, )
+    }
+}
